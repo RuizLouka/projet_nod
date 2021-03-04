3b854720d82f7485226be7821d8e018a1c528748
RuizLouka <39903481+RuizLouka@users.noreply.github.com>
2021-03-04 22:33:51 +0100

    Create projet.js

---
+++ projet.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const mysql = require('mysql');
+
+const fs = require('fs')
+const port = 3000
+const app = express()
+
+
+  connexionbase = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "utilisateur"
+  });
+  const bodyP = require('body-parser');
+  const file = require('express-fileupload')
+  chemin = require('path');
+
+  connexionbase.connect(function (err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+});
+
+app.useapp(bodyP.urlencoded({
+  limit: '500mb',
+  extended: true,
+  
+}));
+app.useapp(file());
+
+app.useapp(express.static(chemin.join(__dirname, 'public')));
+app.post('/utilisateur/', (req, res) => {
+    const valeur = req.body
+   
+    if (!req.files)
+      return res.status(422).json('vous n\'avez pas mis de photo')
+  
+    const file = req.files.image
+    const file_name = new Date().getTime() + '_' + file.name
+    params.image = `image/${file_name}`
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/gif") {
+      file.mv('public/image/' + file_name, (err) => {
+        if (err)
+          return res.status(500).json(err)
+  
+        connexionbase.query('INSERT INTO utilisateur SET ?', [valeur], (err, row) => {
+          if (err) return res.status(500).json(`${err.message}`)
+          console.log(row)
+          res.send(`users avec les parametres ${params.name}, ${params.firstname}, ${params.email}, ${params.date_naissance} ont été ajouté`)
+        })
+      })
+    } else {
+      return res.status(500).json('pas le bon format')
+    }
+  
+  })
+  
+app.put('/utilisateur/:id_utilisateur', (req, res) => {
+    const params = req.body
+  
+    var file = req.files.image
+    const file_name = new Date().getTime() + '_' + file.name
+    params.image = `image/${file_name}`
+  
+    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif") {
+      file.mv('public/image/' + file_name, (err) => {
+        if (err)
+          return res.status(500).json(err)
+  
+        connexionbase.query('UPDATE users SET ? WHERE utilisateur_id = ?', [params, req.params.id_utilisateur], function (err, row) {
+  
+          if (!err) res.json(`update fait`)
+          else console.log(err)
+        })
+      })
+    } else {
+      return res.status(500).json('pas le bon format')
+    }
+  });
+
+
+app.get('/utilisateur', (req, res) => {
+    connexionbase.query('SELECT * FROM utilisateur', function (err, resultat, fields) {
+    if (Object.keys(resultat).length == 0) {
+      res.status(400).json(`aucun utilisateur trouver `)
+    }
+    if (err) res.status(400).send(err)
+    res.json(result);
+  })
+});
+
+app.get('/utilisateur/:id_utilisateur', (req, res, next) => {
+    connexionbase.query('SELECT * FROM utilisateur WHERE utilisateur_id = ?', [req.params.id_utilisateur], 
+    function (err, resultat, fields) {
+    if (Object.keys(resultat).length === 0) {
+      res.status(400).json(`on ne trouve pas  d'utilisateur correspondant `)
+    } else {
+      res.json('L utilisateur ',resultat)
+    }
+    
+  })
+});
+
+app.delete('/utilisateur/:id_utilisateur', (req, res) => {
+  connexionbase.query('DELETE FROM utilisateur WHERE utilisateur_id = ?', [req.params.id_utilisateur],
+   function (err, resultat) {
+        if (err) {
+          res.status(400).json(err)
+        }
+        if (result[0].image !== undefined) fs.unlinkSync(`public/${result[0].image}`)
+        res.json(`l'utilisateur à été supprimé`)
+    })
+  })
+
+
+
+app.listen(port, () => console.log(`connecter au port ${port}`))
