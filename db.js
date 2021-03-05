3d04c28bb9d4f5a06f59f15d4eee5d07f299b0ca
RuizLouka <39903481+RuizLouka@users.noreply.github.com>
2021-03-05 21:05:08 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-connexionbase = mysql.createConnection({host: "localhost",user: "root",password: "root",database: "utilisateur"});
+const connexionbase = mysql.createConnection({host: "localhost",user: "root",password: "root",database: "utilisateur"});
   
 
 module.exports = connexionbase;
