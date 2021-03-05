d8fdfa84d27b10c9014710462a99c5727835e6af
RuizLouka <39903481+RuizLouka@users.noreply.github.com>
2021-03-05 14:49:17 +0100

    Update projet.js

--- projet.js
+++ projet.js
@@ -6,15 +6,7 @@ const port = 3000
 const app = express()
 
 
-  connexionbase = mysql.createConnection({host: "localhost",user: "root",password: "root",database: "utilisateur"});
-  const bodyP = require('body-parser');
-  const file = require('express-fileupload')
-  chemin = require('path');
-
-  connexionbase.connect(function (err) {
-  if (err) throw err;
-  console.log("Connecté à la base de données MySQL!");
-});
+  
 
 app.useapp(bodyP.urlencoded({
   limit: '500mb',
