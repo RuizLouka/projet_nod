4234355271869cdd7c07c255626555de7be2e840
RuizLouka <39903481+RuizLouka@users.noreply.github.com>
2021-03-05 15:17:25 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,4 @@
 connexionbase = mysql.createConnection({host: "localhost",user: "root",password: "root",database: "utilisateur"});
-  const bodyP = require('body-parser');
-  const file = require('express-fileupload')
-  chemin = require('path');
+  
 
 module.exports = connexionbase;
